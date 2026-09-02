96fa792c94b598f08e7457e62b274a4937ae599f
Marine Kitagawa <195569727+kitty4O4@users.noreply.github.com>
2026-09-02 13:23:17 +0600

    Add target.h for SM-M145F with firmware details

---
+++ src/targets/m14-SM-M145FXXU3CXE6/target.h
@@ -0,0 +1,12 @@
+#define TARGET_MODEL "SM-M145F"
+#define TARGET_FIRMWARE "M145FXXU3CXE6"
+#define TARGET_KERNEL_VERSION "5.15.123"
+
+#define OFFSET_commit_creds 0x18b654
+#define OFFSET_prepare_kernel_cred 0x18a3f0
+#define OFFSET_call_usermodehelper_exec_work 0x16ec08
+#define OFFSET_init_task 0xacc5e80
+#define OFFSET_selinux_enforcing 0xab37404
+#define OFFSET_kernel_read 0x557084
+#define OFFSET_do_faccessat 0x550278
+#define OFFSET_core_pattern 0xac56140
